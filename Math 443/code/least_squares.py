b58806f3aef6e88e4ea1a582be08d1e1a14e8777
markstanl <markgstanley1@gmail.com>
2025-03-19 19:21:33 -0500

    session 22

--- Math 443/code/least_squares.py
+++ Math 443/code/least_squares.py
@@ -14,6 +14,19 @@ def compute_least_squares_error(A, b, x):
     return (np.linalg.norm(A @ x - b)) ** 2
 
 
+def weighted_least_squares(A: np.ndarray,
+                           b: np.ndarray,
+                           C: np.ndarray) -> np.ndarray:
+    print(A)
+    print(b)
+    print(C)
+    A_transpose_C_A = A.T @ C @ A
+    A_transpose_C_b = A.T @ C @ b
+    print(A_transpose_C_A)
+    print(A_transpose_C_b)
+    print(np.concatenate((A_transpose_C_A, A_transpose_C_b[:, None]), axis=1))
+    return np.linalg.solve(A_transpose_C_A, A_transpose_C_b)
+
 if __name__ == '__main__':
     A = np.array([[1, 2], [3, -1], [-1, 2], [1, -1], [2, 1]])
     b = np.array([1, 0, -1, 2, 2])
@@ -22,3 +35,12 @@ if __name__ == '__main__':
 
     e = compute_least_squares_error(A, b, x)
     print("Least squares error:", e)
+
+    # weighted least squares
+    A = np.array([[1, 0], [1, 1], [1, 3], [1, 6]])
+    b = np.array([2, 3, 7, 12])
+    C = np.diag([3, 2, 1, 1])
+    x = weighted_least_squares(A, b, C)
+    print(x)
+
+    print(np.linalg.solve(np.array([[2, 5], [10, 46]]), np.array([12, 96])))
